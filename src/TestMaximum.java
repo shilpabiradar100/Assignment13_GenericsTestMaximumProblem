decbf23142aec8bcd741ca55a5acb5135f26d909
shilpa123 <shilpabiradar100@gmail.com>
2023-02-18 15:44:40 +0530

    ADDED: check maximum string using java compare to method

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -1,34 +1,36 @@
 // class to find the maximum from given 3 integers
 public class TestMaximum {
+    public static void main(String[] args) {
 
-        public static void main (String[]args){
-
-            Float number1 = 2.5f;
-            Float number2 = 3.1f;
-            Float number3 = 1.19f;
-            Float maximumNum = testMax(number1, number2, number3);
-            System.out.println("Maximum number is : " + maximumNum);
-        }
-        /**
-         * Method name: testMax
-         * description: It takes three float numbers and calculates max number using compareTo method.
-         * Algorithm: testMax takes three parameter and find max using compareTo. if one number is
-         * greater than other one then compareTo gives positive number(1), lesser than other one it gives
-         * negative(-1).if they are equal it gives 0.
-         * @param number1
-         * @param number2
-         * @param number3
-         * @return maximumNumber
-         */
-        public static Float testMax (Float number1, Float number2, Float number3){
-
-            Float maximumNumber = number1;
-            if (number2.compareTo(maximumNumber) > 0) {
-                maximumNumber = number2;
-            }
-            if (number3.compareTo(maximumNumber) > 0) {
-                maximumNumber = number3;
-            }
-            return maximumNumber;
-        }
+        String name1 = "Apple";
+        String name2 = "Peach";
+        String name3 = "Banana";
+        String maximum = testMax(name1,name2,name3);
+        System.out.println("Maximum length of string is : "+maximum);
     }
+    /**
+     * Method name: testMax
+     * description: It takes three String names and calculates max number using compareTo method by
+     * checking lexicographically.
+     * Algorithm: testMax takes three parameter and find max using compareTo. if one number is
+     * greater than other one then compareTo gives positive number(1), lesser than other one it gives
+     * negative(-1).if they are equal it gives 0.
+     * @param number1
+     * @param number2
+     * @param number3
+     * @return maximumNumber
+     */
+    public static String testMax(String number1, String number2, String number3) {
+
+        String maximumNumber = number1;
+        if(number2.compareTo(maximumNumber) > 0 )
+        {
+            maximumNumber = number2;
+        }
+        if(number3.compareTo(maximumNumber) > 0 )
+        {
+            maximumNumber = number3;
+        }
+        return maximumNumber;
+    }
+}
