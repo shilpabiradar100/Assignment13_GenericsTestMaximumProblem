4c284cf254c0eb7eb4a184e4a2a0fcc5da2e42db
shilpa123 <shilpabiradar100@gmail.com>
2023-02-18 15:39:25 +0530

    ADDED:code to check maximum floating number

--- src/TestMaximum.java
+++ src/TestMaximum.java
@@ -1,34 +1,32 @@
 // class to find the maximum from given 3 integers
 public class TestMaximum {
 
-        public static void main(String[] args) {
+        public static void main (String[]args){
 
-            Integer number1 = 2;
-            Integer number2 = 3;
-            Integer number3 = 5;
-            Integer maximumNum = testMax(number1,number2,number3);
-            System.out.println("Maximum number is : "+maximumNum);
+            Float number1 = 2.5f;
+            Float number2 = 3.1f;
+            Float number3 = 1.19f;
+            Float maximumNum = testMax(number1, number2, number3);
+            System.out.println("Maximum number is : " + maximumNum);
         }
         /**
          * Method name: testMax
-         * description: It takes three integers and calculates max number using compareTo method.
+         * description: It takes three float numbers and calculates max number using compareTo method.
          * Algorithm: testMax takes three parameter and find max using compareTo. if one number is
-         * greater than other than compareTo gives positive number, lesser than other one it gives
-         * negative.if they are equal it gives 0.
+         * greater than other one then compareTo gives positive number(1), lesser than other one it gives
+         * negative(-1).if they are equal it gives 0.
          * @param number1
          * @param number2
          * @param number3
          * @return maximumNumber
          */
-        public static Integer testMax(Integer number1, Integer number2, Integer number3) {
+        public static Float testMax (Float number1, Float number2, Float number3){
 
-            Integer maximumNumber = number1;
-            if(number2.compareTo(maximumNumber) > 0 )
-            {
+            Float maximumNumber = number1;
+            if (number2.compareTo(maximumNumber) > 0) {
                 maximumNumber = number2;
             }
-            if(number3.compareTo(maximumNumber) > 0 )
-            {
+            if (number3.compareTo(maximumNumber) > 0) {
                 maximumNumber = number3;
             }
             return maximumNumber;
